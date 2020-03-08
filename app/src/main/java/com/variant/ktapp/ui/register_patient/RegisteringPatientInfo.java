badc2f0a1b21df738d2f1813da28990230d1e2f0
kasunSomadasa <kasunsomadasa94@gmail.com>
2020-03-08 17:42:10 +0530

    patient registering ui

--- app/src/main/java/com/variant/ktapp/ui/register_patient/RegisteringPatientInfo.java
+++ app/src/main/java/com/variant/ktapp/ui/register_patient/RegisteringPatientInfo.java
@@ -2,15 +2,29 @@ package com.variant.ktapp.ui.register_patient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.variant.ktapp.R;
 
-public class RegisteringPatientInfo extends AppCompatActivity {
+public class RegisteringPatientInfo extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registering_patient_infor);
+
+        btnContinue = findViewById(R.id.btnContinue);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnContinue){
+            startActivity(new Intent(this,RegisteringPatientMedicalHistory.class));
+        }
     }
 }
--- app/src/main/java/com/variant/ktapp/ui/register_patient/RegisteringPatientMedicalHistory.java
+++ app/src/main/java/com/variant/ktapp/ui/register_patient/RegisteringPatientMedicalHistory.java
@@ -2,15 +2,29 @@ package com.variant.ktapp.ui.register_patient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.variant.ktapp.R;
 
-public class RegisteringPatientMedicalHistory extends AppCompatActivity {
+public class RegisteringPatientMedicalHistory extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registering_patient_medical_history);
+
+        btnContinue = findViewById(R.id.btnContinue);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnContinue){
+            startActivity(new Intent(this,RegisteringPatientWound.class));
+        }
     }
 }

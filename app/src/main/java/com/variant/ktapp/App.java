c0eff7a229be07bab458e5ffa6f8ad8f0047b2a9
Chamika Sehan Weerasekara <chamikashe@gmail.com>
2020-03-09 21:03:39 +0530

    new changes

--- app/src/main/java/com/variant/ktapp/App.java
+++ app/src/main/java/com/variant/ktapp/App.java
@@ -1,7 +1,14 @@
 package com.variant.ktapp;
 
 import android.app.Application;
+import android.graphics.Bitmap;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.L;
 import com.variant.ktapp.utils.Settings;
 
 public class App extends Application {
@@ -13,6 +20,26 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
+        initializeUIL();
+    }
+
+
+    private void initializeUIL() {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .defaultDisplayImageOptions(new DisplayImageOptions.Builder()
+                        .cacheOnDisk(true).cacheInMemory(true)
+                        .bitmapConfig(Bitmap.Config.RGB_565)
+                        .considerExifParams(true).build())
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+        if (BuildConfig.DEBUG) {
+            L.writeLogs(true);
+            L.writeDebugLogs(true);
+        }
+        ImageLoader.getInstance().init(config);
     }
 
     public Settings getSettings() {
--- app/src/main/java/com/variant/ktapp/ui/doctor/DoctorDashboardFragment.java
+++ app/src/main/java/com/variant/ktapp/ui/doctor/DoctorDashboardFragment.java
@@ -8,11 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.variant.ktapp.R;
 
 public class DoctorDashboardFragment extends Fragment {
 
+    private ImageView profileImage;
 
     public DoctorDashboardFragment() {
     }
@@ -28,6 +31,12 @@ public class DoctorDashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dash_board, container, false);
+        profileImage = view.findViewById(R.id.profile_image);
+        try {
+            String imgUrl ="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+            ImageLoader.getInstance().displayImage(imgUrl, profileImage);
+        } catch (Exception e) {
+        }
         return view;
     }
 

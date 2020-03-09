534d9c6c3d1efa69b4a4fa66391448c51dbd4530
Chamika Sehan Weerasekara <chamikashe@gmail.com>
2020-03-09 20:23:07 +0530

    new changes in navigation drawer

--- app/src/main/java/com/variant/ktapp/ui/main/MainActivity.java
+++ app/src/main/java/com/variant/ktapp/ui/main/MainActivity.java
@@ -1,10 +1,13 @@
 package com.variant.ktapp.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.transition.Slide;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
 import com.variant.ktapp.R;
@@ -15,12 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
     private ToolBar toolbar;
     private DoctorDashboardFragment doctorDashboardFragment;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View view = findViewById(R.id.toolBar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout.setScrimColor(getResources().getColor(android.R.color.transparent));
         if (view != null) {
             if (view instanceof ToolBar) {
                 toolbar = (ToolBar) view;
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openMenu() {
+        drawerLayout.openDrawer(Gravity.LEFT);
     }
 
     public void showDoctorDashboardFragment() {

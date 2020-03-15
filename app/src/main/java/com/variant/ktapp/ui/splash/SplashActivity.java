682e563d00181849d8c14d3e93844b397c4b0dad
Chamika Sehan Weerasekara <chamikashe@gmail.com>
2020-03-15 15:08:32 +0530

    app restrict to tablet devices

--- app/src/main/java/com/variant/ktapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/variant/ktapp/ui/splash/SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.variant.ktapp.BuildConfig;
 import com.variant.ktapp.R;
@@ -23,11 +24,28 @@ public class SplashActivity extends AppCompatActivity {
         tvVersion = findViewById(R.id.tvVersion);
         tvVersion.setText("ver: "+ BuildConfig.VERSION_NAME);
 
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                finish();
-            }
-        }, 4000);
+        if (isTablet()) {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }, 4000);
+        }else {
+            Toast.makeText(this,"Sorry this application is not supported to mobile devices,Please use a tablet for run this application.",Toast.LENGTH_LONG).show();
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    finish();
+                }
+            }, 4000);
+        }
+    }
+
+    private boolean isTablet() {
+        if (Boolean.parseBoolean(getApplicationContext().getResources().getString(R.string.is_supported_screen))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
